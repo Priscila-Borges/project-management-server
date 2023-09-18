420ea3844947e7976302ebbc5b084b21fdd97b0c
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-18 12:32:11 +0200

    get list of projects + improve error handling

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,5 +1,3 @@
-// routes/project.routes.js
-
 const router = require("express").Router();
 
 // const mongoose = require("mongoose");
@@ -7,13 +5,43 @@ const router = require("express").Router();
 const Project = require("../models/Project.model");
 const Task = require("../models/Task.model");
 
+
 //  POST /api/projects  -  Creates a new project
 router.post("/projects", (req, res, next) => {
-  const { title, description } = req.body;
+    const { title, description } = req.body;
 
-  Project.create({ title, description, tasks: [] })
-    .then(response => res.json(response))
-    .catch(err => res.json(err));
+    const newProject = {
+        title,
+        description,
+        tasks: []
+    }
+
+    Project.create(newProject)
+        .then(response => res.json(response))
+        .catch(err => {
+            console.log("Error creating new project...", err);
+            res.status(500).json({
+                message: "Error creating a new project",
+                error: err
+            });
+        });
 });
 
+
+// GET /api/projects -  Retrieves all of the projects
+router.get('/projects', (req, res, next) => {
+    Project.find()
+        .populate('tasks')
+        .then(allProjects => res.json(allProjects))
+        .catch(err => {
+            console.log("Error getting list of projects...", err);
+            res.status(500).json({
+                message: "Error getting list of projects",
+                error: err
+            });
+        });
+});
+
+
 module.exports = router;
+
